e7e7cd5cf5ae6475e7c56582ee56eb3d79ab052a
Brian Kozlowski <bkozlow1@depaul.edu>
2023-09-30 17:03:13 -0500

    Updated log in page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,12 +1,35 @@
-import { TypographyH2 } from "@/components/typography";
+import { TypographyH2, TypographyP } from "@/components/typography";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 export default function LoginPage() {
     return (
-        <section className={'center'}>
-            
+        <div className={'h-screen flex flex-col items-center justify-center lg:flex-row'}>
+            <section className={'h-screen flex-1 flex items-center justify-center'}>
+                <div className={'w-full flex items-center justify-center flex-col lg:max-w-sm gap-4'}>
 
-        </section>
+                    <TypographyP>
+                        Welcome back! Please log in to your account.
+                    </TypographyP>
+
+                    <TypographyH2>
+                        Log In
+                    </TypographyH2>
+                    <div className={'w-full mt-4'}>
+                        <Input placeholder={'Email'} type="email"/>
+                    </div>
+                    <div className={'w-full mt-4'}>
+                        <Input placeholder={'Password'} type="password"/>
+                    </div>
+                    
+                    <Button className={'w-full text-md'}>
+                        Log In
+                    </Button>
+                </div>
+            </section>
+
+            <section className={'h-screen flex-1 bg-pastel-blue hidden lg:block'}>
+            </section>
+        </div>
     )
 }
