4245caa0e1817a49fb854d80458e449b4cebfba8
brandon-kong <95515854+brandon-kong@users.noreply.github.com>
2023-09-30 14:23:37 -0500

    Added shadcn

--- src/app/join/page.jsx
+++ src/app/join/page.jsx
@@ -1,10 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function JoinPage() {
     return (
-        <section>
-            <div>
-                <h1>
+        <section className={'h-screen flex items-center justify-center'}>
+            <div className={'w-full flex items-center justify-center flex-col max-w-sm gap-4'}>
+                <h1 className={'text-3xl font-bold'}>
                     Join this meeting?
                 </h1>
+                <Input label={'Test'}/>
+                <Button className={'w-full text-md'}>
+                    Join room
+                </Button>
             </div>
         </section>
     )
