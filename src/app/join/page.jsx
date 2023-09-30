6b41f995b5063ad8bd82e1d13dcf77f37459e1a0
brandon-kong <95515854+brandon-kong@users.noreply.github.com>
2023-09-30 14:43:20 -0500

    Added tpyoghrpahy

--- src/app/join/page.jsx
+++ src/app/join/page.jsx
@@ -1,3 +1,4 @@
+import { TypographyH2 } from "@/components/typography";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
@@ -5,9 +6,9 @@ export default function JoinPage() {
     return (
         <section className={'h-screen flex items-center justify-center'}>
             <div className={'w-full flex items-center justify-center flex-col max-w-sm gap-4'}>
-                <h1 className={'text-3xl font-bold'}>
+                <TypographyH2>
                     Join this meeting?
-                </h1>
+                </TypographyH2>
                 <Input label={'Test'}/>
                 <Button className={'w-full text-md'}>
                     Join room
