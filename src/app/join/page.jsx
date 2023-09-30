e21bf47d7daa4043b761abfa15e8c81dcd6e31e2
brandon-kong <95515854+brandon-kong@users.noreply.github.com>
2023-09-30 15:26:18 -0500

    Added join meeting

--- src/app/join/page.jsx
+++ src/app/join/page.jsx
@@ -1,19 +1,33 @@
-import { TypographyH2 } from "@/components/typography";
+import { TypographyH2, TypographyP } from "@/components/typography";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function JoinPage() {
     return (
-        <section className={'h-screen flex items-center justify-center'}>
-            <div className={'w-full flex items-center justify-center flex-col max-w-sm gap-4'}>
-                <TypographyH2>
-                    Join this meeting?
-                </TypographyH2>
-                <Input label={'Test'}/>
-                <Button className={'w-full text-md'}>
-                    Join room
-                </Button>
-            </div>
-        </section>
+        <div className={'h-screen flex flex-col items-center justify-center lg:flex-row'}>
+            <section className={'h-screen flex-1 flex items-center justify-center'}>
+                <div className={'w-full flex items-center justify-center flex-col lg:max-w-sm gap-4'}>
+
+                    <TypographyP>
+                        You are invited to join a meeting by <span className={'font-semibold'}>John Doe</span>
+                    </TypographyP>
+
+                    <TypographyH2>
+                        Join this meeting?
+                    </TypographyH2>
+                    <div className={'w-full mt-8'}>
+                        <Input placeholder={'Name here'}/>
+                    </div>
+                    
+                    <Button className={'w-full text-md'}>
+                        Join room
+                    </Button>
+                </div>
+            </section>
+
+            <section className={'h-screen flex-1 bg-pastel-blue hidden lg:block'}>
+            </section>
+        </div>
     )
 }
