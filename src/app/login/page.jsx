dacf70e6ddde0218cb8fa00b2be432884fa73655
audgeviolin07 <123830780+audgeviolin07@users.noreply.github.com>
2023-09-30 16:45:02 -0500

    nawwwwww

---
+++ src/app/login/page.jsx
@@ -0,0 +1,12 @@
+import { TypographyH2 } from "@/components/typography";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export default function LoginPage() {
+    return (
+        <section className={'center'}>
+            
+
+        </section>
+    )
+}
