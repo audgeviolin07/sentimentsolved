0037f733cf1c2dea112f8c0ef6f87d321461f98e
LightColorz <jojoalemu@gmail.com>
2023-09-30 14:27:23 -0500

    Added blobs and design for homePage

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,20 @@ import Cam from './cam'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-white">
-      <div className='bg-[#EAE4E9] h-[500px] w-full relative top-[175px]'>
-        <div className='bg-gradient from-[#EAE4E9] absolute h-[250px] w-[250px] left-[250px] top-[250px]'>
-
+      <div className='bg-[#EAE4E9] h-[500px] w-full relative top-[175px] overflow-hidden'>
+        <div className='absolute left-[250px] top-[-325px]'>
+          <Image 
+          src={"/blob1.svg"}
+          width={550}
+          height={250}
+          />
+        </div>
+        <div className='absolute left-[950px] top-[250px]'>
+          <Image 
+          src={"/blob2.svg"}
+          width={550}
+          height={250}
+          />
         </div>
       </div>
     </main>
