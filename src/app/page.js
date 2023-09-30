1b10e9934c043154c802ade4fd893493b9e9176c
LightColorz <jojoalemu@gmail.com>
2023-09-30 16:51:06 -0500

    reworked the ui for homePage again

--- src/app/page.js
+++ src/app/page.js
@@ -7,38 +7,19 @@ import { TypographyH1, TypographyH2, TypographyH3, TypographyH4 } from '@/compon
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-white">
-      <div className='border-[1px] h-[500px] w-full relative top-[175px] border-dash'>
-        <div className='absolute left-[250px] top-[-300px] min-w-[550px] '>
-          <Image 
-          src={'/blob2.svg'}
-          width={550}
-          height={250}
-          />
-        </div>
-        <div className='absolute left-[450px] top-[350px] min-w-[550px]'>
-          <Image 
-          src={'/blob (1).svg'}
-          width={550}
-          height={250}
-          />
-        </div>
-
-        <div className='absolute left-[1000px] top-[200px] min-w-[550px]'>
-          <Image 
-          src={'/blob1.svg'}
-          width={550}
-          height={250}/>
-        </div>
-        <div className='bg-[#E2ECE9] absolute left-[300px] h-[150px] w-[150px] top-[50px] rounded-full'>
-        </div>
+      <div className='border-[1px] h-[500px] w-full min-w-full relative top-[175px] border-dash overflow-hidden'>
+        <div className='bg-[#EAE4E9] absolute h-[750px] w-[750px] left-[250px] top-[-300px] rounded-full blur-lg opacity-40'></div>
+        <div className='bg-[#FFF1E6] absolute h-[750px] w-[750px] left-[450px] top-[350px] rounded-full blur-lg opacity-20'></div>
+        <div className='bg-[#FDE2E4] absolute h-[750px] w-[750px] left-[1000px] top-[200px] rounded-full blur-lg opacity-20'></div>
+        <div className='bg-[#E2ECE9] absolute h-[750px] w-[750px] left-[500px] top-[50px] rounded-full blur-lg opacity-20'></div>
+        <div className='bg-[#FAD2E1] absolute h-[750px] w-[750px] left-[0px] top-[150px] rounded-full blur-lg opacity-20'></div>
+        <div className='bg-[#CDDAFD] absolute h-[750px] w-[750px] left-[1450px] top-[-150px] rounded-full blur-lg opacity-20'></div>
         <Cam />
         <Link href="/cam">What</Link>
         <div className='flex absolute flex-col items-center w-full justify-center top-[150px] gap-16'>
           <TypographyH2>Expression AI</TypographyH2>
-          
           <TypographyH3>An AI made to help people with ASD understand emotions</TypographyH3>
         </div>
-        
       </div>
     </main>
   )
