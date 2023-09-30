bd310a56c8a09db469e8072caf84fd2adc45ce07
brandon-kong <95515854+brandon-kong@users.noreply.github.com>
2023-09-30 13:25:24 -0500

    Removed body tag in css, join page

---
+++ src/app/join/page.jsx
@@ -0,0 +1,11 @@
+export default function JoinPage() {
+    return (
+        <section>
+            <div>
+                <h1>
+                    Join this meeting?
+                </h1>
+            </div>
+        </section>
+    )
+}
