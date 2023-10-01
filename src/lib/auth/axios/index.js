3d9cb5788bb222540a60cc024d4883dfac707e92
brandon-kong <95515854+brandon-kong@users.noreply.github.com>
2023-10-01 00:59:43 -0500

    test

---
+++ src/lib/auth/axios/index.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import { getSession } from 'next-auth/react';
+import { getServerSession } from "next-auth";
+import authOptions from '../next-auth/authOptions';
+
+const api = axios.create({
+    baseURL: process.env.BACKEND_API_URL,
+});
+
+const clientApi = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_API_URL,
+    headers: {
+        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    }
+});
+
+const localFetcherGet = async (url) => {
+    const session = await getSession();
+    const token = session?.user.access;
+
+    return axios.get(url).then(res => res.data);
+};
+
+export { api, clientApi, localFetcherGet };
+
+export const AuthenticatedRequest = async (url, method, data) => {
+    const session = await getServerSession(authOptions);
+    const token = session?.user.access;
+
+    if (!token) {
+        const B = {
+            status_code: 401,
+            error_type: 'invalid_token',
+            error_message: 'Invalid token',
+            detail: 'Invalid token',
+        }  
+
+        return B;
+    }
+
+    return api({
+        method: method,
+        url: url,
+        data: data,
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    }).then(res => res.data).catch(err => err.response.data);
+}
---
+++ src/lib/auth/next-auth/authOptions/index.js
@@ -0,0 +1,210 @@
+import GoogleProvider from 'next-auth/providers/google';
+import CredentialProvider from 'next-auth/providers/credentials';
+
+import { api } from '@/lib/auth/axios';
+import { refreshToken } from '@/lib/auth/util';
+
+import jwtDecode from 'jwt-decode';
+
+const authOptions = {
+    secret: process.env.NEXTAUTH_SECRET,
+    session: {
+        strategy: 'jwt',
+    },
+
+    pages: {
+        signIn: '/?login=1',
+        signOut: '/?logout=1',
+        error: '/?login=1&auth_error=1',
+    },
+    
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+
+            authorization: {
+                params: {
+                    prompt: 'consent',
+                    access_type: 'offline',
+                    response_type: 'code',
+                },
+            },
+        }),
+
+        CredentialProvider({
+            id: 'email-password',
+            name: 'Credentials',
+            credentials: {
+                email: {
+                    label: 'Email',
+                    type: 'text',
+                    placeholder: 'Email',
+                },
+                password: {
+                    label: 'Password',
+                    type: 'password',
+                    placeholder: 'Password',
+                },
+            },
+
+            async authorize(credentials, req) {
+                const { email, password } = credentials;
+                try {
+                    const { data, status } = await api.post('/accounts/verify/email/', {
+                        email,
+                        password,
+                    });
+
+                    const detail = data.detail;
+                    if (status === 200) {
+                        return {
+                            access: detail.access,
+                            refresh: detail.refresh,
+                            //...detail.user,
+                        };
+                    }
+
+                    return null;
+                } catch (error) {
+                    return null;
+                }
+            },
+        }),
+
+        CredentialProvider({
+            id: 'phone-otp',
+            name: 'Phone OTP',
+            credentials: {
+                phone: {
+                    label: 'Phone',
+                    type: 'text',
+                    placeholder: 'Phone',
+                },
+                token: {
+                    label: 'Token',
+                    type: 'text',
+                    placeholder: 'Token',
+                },
+                country_code: {
+                    label: 'Country Code',
+                    type: 'text',
+                    placeholder: 'Country Code',
+                },
+            },
+
+            async authorize(credentials, req) {
+                const { phone, token, country_code } = credentials;
+
+                const phoneWithCountryCode = country_code + phone;
+
+                try {
+                    const { data, status } = await api.post('/accounts/verify/phone/', {
+                        phone: phoneWithCountryCode,
+                        token,
+                    });
+
+                    const detail = data.detail;
+
+                    if (status === 200) {
+                        return {
+                            access: detail.access,
+                            refresh: detail.refresh,
+                            ...detail.user,
+                        };
+                    }
+
+                    return null;
+                } catch (error) {
+                    return null;
+                }
+            },
+        }),
+    ],
+
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                if (!user.access || !user.refresh) {
+                    return token;
+                }
+                token.access = user.access;
+                token.refresh = user.refresh;
+
+                token.user = user;
+            }
+
+            const tokenAccess = token.access;
+            const tokenRefresh = token.refresh;
+
+            const decodedAccess = jwtDecode<JWT>(tokenAccess);
+            const decodedRefresh = jwtDecode<JWT>(tokenRefresh);
+
+            if (decodedAccess.exp * 1000 > Date.now()) {
+                return Promise.resolve(token);
+            }
+
+            if (decodedRefresh.exp * 1000 < Date.now()) {
+                return null;
+            }
+
+            const newToken = await refreshToken(token);
+            if (newToken) {
+                newToken.user = token.user;
+                newToken.user.access = newToken.access;
+                newToken.user.refresh = newToken.refresh;
+
+                return newToken;
+            }
+        },
+
+        async session({ session, token, user }) {
+            if (!token) {
+                return null;
+            }
+            
+            session.access = token.access;
+            session.refresh = token.refresh;
+            session.error = token.error;
+            
+
+            session.user = token.user;
+            return session;
+        },
+
+        async signIn({ user, account, profile, email, credentials, callback }) {
+            if (credentials) {
+
+                return true;
+            }
+
+            if (account) {
+                const { access_token, id_token, provider } = account;
+                try {
+                    const response = await api.post('/accounts/verify/oauth/google/', {
+                        access_token: id_token,
+                    });
+
+                    if (response.status === 200) {
+                        account.user = response.data.user;
+                        user.first_name = response.data.user.first_name;
+                        user.last_name = response.data.user.last_name;
+                        user.pk = response.data.user.pk;
+                        user.access = response.data.access;
+                        user.refresh = response.data.refresh;
+
+                        return true;
+                    }
+
+                    return '/?login=1&auth_error=oauth';
+                } catch (error) {
+                    return '/?login=1&auth_error=oauth';
+                }
+            }
+
+            return false;
+        },
+    },
+};
+
+export default authOptions;
