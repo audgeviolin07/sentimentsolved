64827f84a2c492788478e1b9b20ff95f856b5ee6
LightColorz <jojoalemu@gmail.com>
2023-09-30 19:48:39 -0500

    added livekit. Try to make it so you can create a livekit room and share the link

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={open_sans.className}>
-        <Navbar/>
         {children}
       </body>
     </html>
